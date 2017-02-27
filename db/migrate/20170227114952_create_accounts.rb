db736b1f3cb83e6359dee4810ef44ff21afd9a05
Fabian Jahr <fabian.jahr@whu.edu>
2017-02-27 13:29:58 +0100

    account association is created on registration

--- db/migrate/20170227114952_create_accounts.rb
+++ db/migrate/20170227114952_create_accounts.rb
@@ -1,6 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :accounts do |t|
+      t.belongs_to :user, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,10 @@ ActiveRecord::Schema.define(version: 20170227114952) do
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id", using: :btree
   end
 
   create_table "active_admin_comments", force: :cascade do |t|
