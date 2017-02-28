749539474452d1e653cb0d948b062790e176b3f0
Fabian Jahr <fabian.jahr@whu.edu>
2017-02-28 23:42:18 +0100

    add tipps for alexa interaction model and response

--- app/services/alexa/front_desk_interaction_model.rb
+++ app/services/alexa/front_desk_interaction_model.rb
@@ -1,3 +1,19 @@
+#Rules for Sample Utterances
+#While contents of any custom slot values can be entered in different forms (digits, abbreviations, etc.), the surrounding sample utterances need to follow certain rules, as follows. These rules also apply to any words specified inline in AMAZON.LITERAL slot values.
+#Numbers should be expressed as words and not digits (“five”, not “5”).
+#Acronyms and other phrases involving spoken letters should be separated by periods and spaces (“n. b. a.”, not “nba”).
+#Punctuation marks should not be included except in the special cases listed below. (“ten dollars”, not “$10”. “three point five stars”, not “3.5 stars”). Exceptions:
+#Include periods after letter abbreviations (“n. b. a.”, “e. t. a.”).
+#Include apostrophes in possessive and contractions (“romeo’s” and “i’m”).
+#Include hyphens that are word-internal (“man-eating”) but in no other cases.
+#If the word for a slot value may have apostrophes indicating the possessive, or any other similar punctuation (such as periods or hyphens) include those within the brackets defining the slot. Do not add 's after the closing bracket. For example:
+#For possessives
+#Use: "martini's" as a custom value for {Drink} and "tell me a {Drink} ingredients" in the sample utterances
+#Do not use: "tell me a {Drink}'s ingredients"
+#For hyphenated values
+#Use: editor-in-chief in the custom values and {Position} in the sample utterances
+#Do not use: editor in the custom values and {Position}-in-chief in the sample utterances
+
 class Alexa::FrontDeskInteractionModel
   def self.build
     AlexaGenerator::InteractionModel.build do |model|
--- app/services/alexa/response.rb
+++ app/services/alexa/response.rb
@@ -7,12 +7,18 @@ class Alexa::Response
     resp = AlexaRubykit::Response.new
 
     case intent_name
+    when "LAUNCH_REQUEST"
+                # user talked to our skill but did not say something matching intent
+          #       message = "Say something see what happens."
     when "NotifyArrival"
       resp.add_speech('Implement me')
     when "AMAZON.HelpIntent"
       resp.add_speech(
         "Let me know who you are and who you are looking to meet. If you don't know who you want to meet, just say anyone."
       )
+    when "SESSION_ENDED_REQUEST"
+            # it's over
+      #       message = nil
     else
       resp.add_speech('Sorry, something went wrong. Please try again.')
     end
