090c0f43d27e0a2a351d24e90cde88416822f5b8
Fabian Jahr <fabian.jahr@whu.edu>
2017-03-03 21:39:03 +0100

    add validations to alias

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,3 +1,6 @@
 class Alias < ApplicationRecord
   belongs_to :member
+
+  validates :name, presence: true
+  validates :member_id, presence: true
 end
--- spec/models/alias_spec.rb
+++ spec/models/alias_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Alias, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:attr) { {member_id: 1, name: "John Doe"} }
+
+  subject { Alias.new(attr) }
+
+  describe "with a name and a member association" do
+    it "should be valid" do
+      expect(subject).to be_valid
+    end
+  end
+
+  describe "without a name" do
+    let(:attr) { {member_id: 1, name: nil} }
+
+    it "should not be valid" do
+      expect(subject).not_to be_valid
+    end
+  end
+
+  describe "without a member association" do
+    let(:attr) { {member_id: nil, name: "John Doe"} }
+
+    it "should not be valid" do
+      expect(subject).not_to be_valid
+    end
+  end
 end
