f073e7ad79ab70ec5a4f38c6f7067ec7c84cc826
Fabian Jahr <fabian.jahr@whu.edu>
2017-03-22 20:12:14 +0100

    many improvements on the homepage

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.middleware.use RackPassword::Block, auth_codes: ['kilocorn']
+  config.middleware.use RackPassword::Block, auth_codes: ['westworld']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
