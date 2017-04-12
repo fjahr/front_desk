dfbc1d7090dd8b2771984a0354e3cfbc3a29c031
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-13 01:59:33 +0200

    change requirement sequence

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require tether
 //= require bootstrap-sprockets
-//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
