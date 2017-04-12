26c92a0d0fb3c80eaf809f91ac89eceef342a098
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-13 00:37:37 +0200

    add slack member importer

---
+++ app/services/slack_importer.rb
@@ -0,0 +1,35 @@
+class SlackImporter
+  def initialize(account)
+    @account = account
+    @client = Slack::Web::Client.new(token: account.slack_token)
+  end
+
+  def run
+    import_slack_team_members
+  end
+
+  private
+
+  def import_slack_team_members
+    resp = @client.users_list
+    members = resp["members"]
+
+    members.each do |member|
+      import_member(member) unless is_bot?(member)
+    end
+  end
+
+  def is_bot?(member)
+    member["is_bot"] == true || member["id"] == "USLACKBOT"
+  end
+
+  def import_member(member)
+    m = Member.find_or_initialize_by(slack_id: member["name"])
+    unless m.persisted?
+      m.name = member["profile"]["real_name"]
+      m.email = member["profile"]["email"]
+      m.phone = member["profile"]["phone"]
+      m.save
+    end
+  end
+end
