9292a5b2f86030125d5d40898772afda7f7b32ae
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-13 02:49:40 +0200

    kill me

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require tether
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require_tree .
