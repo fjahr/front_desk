d8cb72494751fd0d3844d7ec389ffa574630d41d
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-13 02:12:56 +0200

    another try of correct assets config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= require bootstrap
 //= require tether
 //= require turbolinks
 //= require_tree .
