0ca93880e65614b882a758cce4f81395417f2b2f
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-13 20:32:24 +0200

    add explicit dropdown activation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require tether
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  $('.dropdown-toggle').dropdown();
+});
