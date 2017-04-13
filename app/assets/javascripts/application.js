22e1a7dc51cebacc2aa65d12eefc511e1c996083
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-13 02:05:16 +0200

    another try with assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require tether
-//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
