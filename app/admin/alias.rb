9805bc2aec30ac7da6e70b8eb84cd43b35daf8d1
Fabian Jahr <fabian.jahr@whu.edu>
2017-05-04 00:07:01 +0200

    permit params for alias in active admin

--- app/admin/alias.rb
+++ app/admin/alias.rb
@@ -2,8 +2,8 @@ ActiveAdmin.register Alias do
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
+
+permit_params :member_id, :name
 #
 # or
 #
